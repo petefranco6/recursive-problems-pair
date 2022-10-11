808dbbf9caae7478f282578ad631fbebf4191154
DESKTOP-FDHA9LS\petet <petethe4th@live.com>
2022-10-11 11:27:07 -0500

    02-pete-fix

--- problems/02-sum-array.js
+++ problems/02-sum-array.js
@@ -12,17 +12,23 @@ sumArray([1, 2, 3, 4, 5]); // 15
 
 function sumArray(arr) {
   // Your code here
-  if(arr.length > 1){
-    return (arr[0] + sumArray(arr.slice(1)));
-  } else {
-    return arr[0]
-  }
+//   if(arr.length > 1){
+//     return (arr[0] + sumArray(arr.slice(1)));
+//   } else {
+//     return arr[0]
+//   }
+// }
+if(arr.length < 1) {
+  return 0;
+}
+let sum = arr.pop() + sumArray(arr);
+return sum;
 }
 
 console.log(sumArray([1, 2, 3])); //  6
 console.log(sumArray([0, 1, -3])); //  -2
 console.log(sumArray([1, 2, 3, 4, 5])); // 15
-  
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
