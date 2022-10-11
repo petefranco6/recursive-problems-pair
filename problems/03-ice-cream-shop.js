a596b77c5510ef138c175202264eeccdf35e6d86
Hubert Hwang <huberthwang919@gmail.com>
2022-10-11 10:09:22 -0700

    03-ice-cream-shop

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -15,8 +15,23 @@ iceCreamShop([], 'honey lavender'); // false
 
 function iceCreamShop(flavors, favorite) {
   // Your code here
+  if(flavors.length < 1){
+    return false;
+  } else {
+    if(flavors[0] === favorite){
+      return true;
+    }
+  else {
+return iceCreamShop(flavors.slice(1), favorite)
+  }
+}
 }
 
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
+console.log(iceCreamShop([], 'honey lavender')); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
